77296d38c2ef2d58d6053f2603746776de4c5b97
SinonJZH <44700327+SinonJZH@users.noreply.github.com>
2019-07-05 06:15:26 +0800

    Create admin_page.js

---
+++ js/admin_page.js
@@ -0,0 +1,7 @@
+function add_one_process(id){
+    function add_one_process(id){
+        var prog=document.getElementById(id).value;
+        prog++;
+        prog=document.getElementById(id).value=prog;
+    }
+}
