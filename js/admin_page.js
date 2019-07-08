082caa4d85020812fedbcdd906784a161d425a5c
SinonJZH <sinon.jzh@gmail.com>
2019-07-08 14:52:08 +0800

    完成进度+1按钮的添加

--- js/admin_page.js
+++
@@ -1,7 +0,0 @@
-function add_one_process(id){
-    function add_one_process(id){
-        var prog=document.getElementById(id).value;
-        prog++;
-        prog=document.getElementById(id).value=prog;
-    }
-}
