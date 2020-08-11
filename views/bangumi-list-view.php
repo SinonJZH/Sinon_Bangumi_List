a396a56b915599c8839c568c2ddcb9b6bc1d3198
SamLangTen <samlangten@outlook.com>
2020-08-11 22:18:53 +0800

    finish status update

--- views/bangumi-list-view.php
+++ views/bangumi-list-view.php
@@ -5,7 +5,14 @@
         $all_bangumi = get_option("sinonbangumilist_savedbangumi");
         $bangumi = $all_bangumi[$_POST['id']];
         //Update
-        if ((int)$_POST['bg_status']!=1) {
+        if ((int)$_POST['bg_status']==1) {
+            //if in Watching, update progress and times
+            if ($_POST['progress']!=null) {
+                $bangumi['progress'] = (int)sanitize_text_field($_POST['progress']);
+            }
+            if ($_POST['times']!=null) {
+                $bangumi['times'] = (int)sanitize_text_field($_POST['times']);
+            }
         }
         $bangumi['status'] = (int)sanitize_text_field($_POST['bg_status']);
         $all_bangumi[$_POST['id']] = $bangumi;
@@ -31,32 +38,41 @@
                 } else {
                     foreach ($all_bangumi as $bangumi) {
                         ?>
-                        <tr>
-                            <th>
-                                <strong><a class="row-title"><?php echo($bangumi["name_cn"]) ?></a></strong>
-                                <div class="row-actions">
-                                        <span class="edit">
-                                            <a href="<?php echo(admin_url()."admin.php?page=sinon_bangumi_new&bangumi_id=".$bangumi["id"]); ?>"><?php _e("Edit", "sinon-bangumi-list"); ?></a> | 
-                                        </span>
-                                        <span class="delete">
-                                            <a href="<?php echo(admin_url()."admin.php?page=sinon_bangumi_new&bangumi_id=".$bangumi["id"]."&action=delete"); ?>"><?php _e("Delete", "sinon-bangumi-list"); ?></a>
-                                        </span>
-                                </div>
-                            </th>
-                            <td>
-                                <form action="" method="POST">
-                                    <input type="hidden" value="update_status" name="action"/>
-                                    <input type="hidden" value="<?php echo($bangumi["id"]) ?>" name="id"/>
-                                    <select name="bg_status">
-                                        <option value=0 <?php echo($bangumi['status']==0?"selected":""); ?>><?php echo(_e("Ready to Watch", "sinon-bangumi-list")); ?></option>
-                                        <option value=1 <?php echo($bangumi['status']==1?"selected":""); ?>><?php echo(_e("In Watching", "sinon-bangumi-list")); ?></option>
-                                        <option value=2 <?php echo($bangumi['status']==2?"selected":""); ?>><?php echo(_e("Watched", "sinon-bangumi-list")); ?></option>
-                                    </select>
-                                    <input type="submit" value="<?php echo(_e("Update Status", "sinon-bangumi-list")); ?>" class="button button-primary">
-                                </form>
-                            </td>
-                        </tr>
+            <tr>
+                <th>
+                    <strong><a class="row-title"><?php echo($bangumi["name_cn"]) ?></a></strong>
+                    <div class="row-actions">
+                            <span class="edit">
+                                <a href="<?php echo(admin_url()."admin.php?page=sinon_bangumi_new&bangumi_id=".$bangumi["id"]); ?>"><?php _e("Edit", "sinon-bangumi-list"); ?></a> | 
+                            </span>
+                            <span class="delete">
+                                <a href="<?php echo(admin_url()."admin.php?page=sinon_bangumi_new&bangumi_id=".$bangumi["id"]."&action=delete"); ?>"><?php _e("Delete", "sinon-bangumi-list"); ?></a>
+                            </span>
+                    </div>
+                </th>
+                <td>
+                    <form action="" method="POST">
+                        <input type="hidden" value="update_status" name="action"/>
+                        <input type="hidden" value="<?php echo($bangumi["id"]) ?>" name="id"/>
+                        <select name="bg_status">
+                            <option value=0 <?php echo($bangumi['status']==0?"selected":""); ?>><?php echo(_e("Ready to Watch", "sinon-bangumi-list")); ?></option>
+                            <option value=1 <?php echo($bangumi['status']==1?"selected":""); ?>><?php echo(_e("In Watching", "sinon-bangumi-list")); ?></option>
+                            <option value=2 <?php echo($bangumi['status']==2?"selected":""); ?>><?php echo(_e("Watched", "sinon-bangumi-list")); ?></option>
+                        </select>
                         <?php
+                            if ($bangumi['status']==1) {
+                                ?>
+                        <label for="progress"><?php echo(_e("Progress:", "sinon-bangumi-list")); ?></label>
+                        <input type="text" value="<?php echo($bangumi["progress"]) ?>" name="progress" style="width: 40px;text-align: center;"/>
+                        <label for="times"><?php echo(_e("Times:", "sinon-bangumi-list")); ?></label>
+                        <input type="text" value="<?php echo($bangumi["times"]) ?>" name="times" style="width: 40px;text-align: center;"/>
+                                <?php
+                            } ?>
+                        <input type="submit" value="<?php echo(_e("Update Status", "sinon-bangumi-list")); ?>" class="button button-primary">
+                    </form>
+                </td>
+            </tr>
+            <?php
                     }
                 }
             ?>
